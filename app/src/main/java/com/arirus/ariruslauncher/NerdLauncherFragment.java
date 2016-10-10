29bfe7da2aa8453705214deabf1a232bc630fc71
Arirus <whd910421@hotmail.com>
2016-10-10 14:29:12 +0800

    Launcher app 搭框架

---
+++ app/src/main/java/com/arirus/ariruslauncher/NerdLauncherFragment.java
@@ -0,0 +1,52 @@
+package com.arirus.ariruslauncher;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by whd910421 on 16/10/9.
+ */
+
+public class NerdLauncherFragment extends Fragment {
+    private static final String TAG = "NerdLauncherFragment";
+
+    private RecyclerView mRecyclerView;
+
+    public static NerdLauncherFragment newInstance()
+    {
+        return new NerdLauncherFragment();
+    }
+
+    @Nullable
+    @Override//仅仅创建出来
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_nerd_launcher,container,false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.fragment_nerd_launcher_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        setupAdapter();
+        return v;
+    }
+
+    private void setupAdapter()
+    {
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent,0);
+
+        Log.i(TAG, "Found " + activities.size() + " activities.");
+    }
+}
